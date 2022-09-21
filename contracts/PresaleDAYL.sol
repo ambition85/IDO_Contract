1ecbc886f12554fbb5f10302751cf37a61eb6d41
ambiton85 <ambitoin9385@gmail.com>
2022-09-21 16:19:25 +0800

    premint 7.3m

--- contracts/PresaleDAYL.sol
+++ contracts/PresaleDAYL.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PresaleDAYL is ERC20, Ownable {
-    constructor() ERC20("Project Daylight", "pDAYL") {}
 
+    constructor() ERC20("Project Daylight", "pDAYL") {
+        _mint(msg.sender, 7200000 * 1e18);
+    }
 }
