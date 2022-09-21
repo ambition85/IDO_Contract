93a5ae65b6f7bd9dde257b6d46804e1bf9fd9a75
ambiton85 <ambitoin9385@gmail.com>
2022-09-21 15:46:25 +0800

    remove presale address variable

--- contracts/PresaleDAYL.sol
+++ contracts/PresaleDAYL.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PresaleDAYL is ERC20, Ownable {
-    address public presale;
-
     constructor() ERC20("Project Daylight", "pDAYL") {}
 
 }
